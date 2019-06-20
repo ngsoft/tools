7b990b1f610c06eaf91a0720d0187d6e7c278fea
Aymeric Anger <daedelus.git@gmail.com>
2019-06-21 01:53:12 +0200

    Working on OPCache

--- src/Cache/CacheItemAbstract.php
+++ src/Cache/CacheItemAbstract.php
@@ -13,97 +13,54 @@ use Psr\Log\LoggerAwareTrait;
 class CacheItemAbstract implements CacheItemInterface {
 
     /**
-     * @var string
+     * @var CacheManager
      */
-    protected $key;
+    private $manager;
 
-    /**
-     * @var mixed
-     */
-    protected $value;
-
-    /**
-     * @var boolean
-     */
-    protected $hit;
-
-    /**
-     * @var DateTime
-     */
-    protected $expiration;
-
-    /**
-     * Constructs a new CacheItem.
-     *
-     * @param string $key
-     *   The key of the cache item this object represents.
-     * @param array $data
-     *   An associative array of data from the Memory Pool.
-     */
-    public function __construct(\Psr\Cache\CacheItemPoolInterface $key, array $data) {
-        $this->key = $key;
-        $this->value = $data['value'];
-        $this->expiration = $data['ttd'];
-        $this->hit = $data['hit'];
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function getKey() {
-        return $this->key;
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function get() {
-        return $this->isHit() ? $this->value : null;
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function set($value = null) {
-        $this->value = $value;
-        return $this;
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function isHit() {
-        return $this->hit;
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function expiresAt($expiration) {
-        if (is_null($expiration)) {
-            $this->expiration = new DateTime('now +1 year');
-        } else {
-            assert($expiration instanceof DateTimeInterface);
-            $this->expiration = $expiration;
-        }
-        return $this;
+    public function __construct(CacheManager $manager) {
+        $this->manager = $manager;
     }
 
     /**
      * {@inheritdoc}
      */
     public function expiresAfter($time) {
-        if (is_null($time)) {
-            $this->expiration = new DateTime('now +1 year');
-        } elseif (is_numeric($time)) {
-            $this->expiration = new DateTime('now +' . $time . ' seconds');
-        } else {
-            assert($time instanceof DateInterval);
-            $expiration = new DateTime();
-            $expiration->add($time);
-            $this->expiration = $expiration;
-        }
-        return $this;
+
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function expiresAt($expiration) {
+
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function get() {
+
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getKey() {
+
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function isHit() {
+
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function set($value) {
+
     }
 
     /**
@@ -118,7 +75,7 @@ class CacheItemAbstract implements CacheItemInterface {
      * @internal
      */
     public function getExpiration() {
-        return $this->expiration ?: new \DateTime('now +1 year');
+        //return $this->expiration ?: new \DateTime('now +1 year');
     }
 
     /**
@@ -132,36 +89,7 @@ class CacheItemAbstract implements CacheItemInterface {
      * @internal
      */
     public function getRawValue() {
-        return $this->value;
-    }
 
-    /**
-     * Returns the expiration timestamp.
-     *
-     * Although not part of the CacheItemInterface, this method is used by
-     * the pool for extracting information for saving.
-     *
-     * @return \DateTime
-     *   The timestamp at which this cache item should expire.
-     *
-     * @internal
-     */
-    public function getExpiration() {
-        return $this->expiration ?: new \DateTime('now +1 year');
-    }
-
-    /**
-     * Returns the raw value, regardless of hit status.
-     *
-     * Although not part of the CacheItemInterface, this method is used by
-     * the pool for extracting information for saving.
-     *
-     * @return mixed
-     *
-     * @internal
-     */
-    public function getRawValue() {
-        return $this->value;
     }
 
 }
---
+++ src/Cache/SimpleCacheAdapter.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace NGSOFT\Tools\Cache;
+
+use Psr\SimpleCache\CacheInterface;
+
+class SimpleCacheAdapter implements CacheInterface {
+
+    public function clear() {
+
+    }
+
+    public function delete($key) {
+
+    }
+
+    public function deleteMultiple($keys) {
+
+    }
+
+    public function get($key, $default = null) {
+
+    }
+
+    public function getMultiple($keys, $default = null) {
+
+    }
+
+    public function has($key) {
+
+    }
+
+    public function set($key, $value, $ttl = null) {
+
+    }
+
+    public function setMultiple($values, $ttl = null) {
+
+    }
+
+}
