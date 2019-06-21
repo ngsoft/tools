71a33a0db3469f6a63fc899007ad2c098605f444
Aymeric Anger <daedelus.git@gmail.com>
2019-06-21 05:34:38 +0200

    Working on OPCache

--- src/Cache/SimpleCacheAdapter.php
+++ src/Cache/SimpleCacheAdapter.php
@@ -17,56 +17,84 @@ class SimpleCacheAdapter implements CacheInterface {
      * {@inheritdoc}
      */
     public function clear() {
-
+        return $this->pool->clear();
     }
 
     /**
      * {@inheritdoc}
      */
     public function delete($key) {
+        try {
 
+        } catch (\Throwable $ex) {
+            throw new PSRCacheInvalidKey($ex->getMessage());
+        }
     }
 
     /**
      * {@inheritdoc}
      */
     public function deleteMultiple($keys) {
+        try {
 
+        } catch (\Throwable $ex) {
+            throw new PSRCacheInvalidKey($ex->getMessage());
+        }
     }
 
     /**
      * {@inheritdoc}
      */
     public function get($key, $default = null) {
+        try {
 
+        } catch (\Throwable $ex) {
+            throw new PSRCacheInvalidKey($ex->getMessage());
+        }
     }
 
     /**
      * {@inheritdoc}
      */
     public function getMultiple($keys, $default = null) {
+        try {
 
+        } catch (\Throwable $ex) {
+            throw new PSRCacheInvalidKey($ex->getMessage());
+        }
     }
 
     /**
      * {@inheritdoc}
      */
     public function has($key) {
+        try {
 
+        } catch (\Throwable $ex) {
+            throw new PSRCacheInvalidKey($ex->getMessage());
+        }
     }
 
     /**
      * {@inheritdoc}
      */
     public function set($key, $value, $ttl = null) {
+        try {
 
+        } catch (\Throwable $ex) {
+            throw new PSRCacheInvalidKey($ex->getMessage());
+        }
     }
 
     /**
      * {@inheritdoc}
      */
     public function setMultiple($values, $ttl = null) {
+        try {
 
+        } catch (\Throwable $ex) {
+            throw new PSRCacheInvalidKey($ex->getMessage());
+        }
     }
 
 }
