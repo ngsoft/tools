0c9de01b9539e6eb77b2497fd76463b2106818e9
Aymeric Anger <daedelus.git@gmail.com>
2019-06-21 02:28:45 +0200

    Working on OPCache

---
+++ src/Traits/ContainerAware.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace NGSOFT\Tools\Traits;
+
+use Psr\Container\ContainerInterface;
+
+trait ContainerAware {
+
+    /**
+     * @var ContainerInterface|null
+     */
+    protected $container;
+
+    /**
+     * Get Item from Container
+     * @param string $key
+     * @return mixed
+     */
+    protected function get(string $key) {
+        return isset($this->container) ? $this->container->get($key) : null;
+    }
+
+    /**
+     * Checks if container has item
+     * @param string $key
+     * @return bool
+     */
+    protected function has(string $key): bool {
+        return isset($this->container) ? $this->container->has($key) : false;
+    }
+
+    /**
+     * Inject the Container
+     * @param ContainerInterface $container
+     */
+    public function setContainer(ContainerInterface $container) {
+        $this->container = $container;
+    }
+
+}
