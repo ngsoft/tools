c8e61bfcb8c5946f474ad549117c8aaac18be1e3
Aymeric Anger <daedelus.git@gmail.com>
2019-06-22 01:52:38 +0200

    Working on OPCache

--- src/Objects/Collection.php
+++ src/Objects/Collection.php
@@ -3,7 +3,26 @@
 namespace NGSOFT\Tools\Objects;
 
 use Illuminate\Support\Collection as LaravelCollection;
+use function NGSOFT\Tools\array_every;
 
 class Collection extends LaravelCollection {
+
+    /**
+     * Test if all elements of the collection pass the given test
+     * @param callable $callback
+     * @return bool
+     */
+    public function every(callable $callback): bool {
+        return array_every($callback, $this->items);
+    }
+
+    /**
+     * Test if at least one element of the collection pass the given test
+     * @param callable $callback
+     * @return bool
+     */
+    public function some(callable $callback): bool {
+        return !$this->every($callback);
+    }
 
 }
