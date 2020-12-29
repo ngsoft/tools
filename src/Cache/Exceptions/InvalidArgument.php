0defea4723a616f37b79686c1e8137477fb35942
Aymeric Anger <daedelus.git@gmail.com>
2020-12-29 18:01:43 +0100

    composer

--- src/Cache/Exceptions/InvalidArgument.php
+++ src/Cache/Exceptions/InvalidArgument.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace NGSOFT\Tools\Cache\Exceptions;
 
 class InvalidArgument extends \InvalidArgumentException implements \Psr\Cache\InvalidArgumentException {
